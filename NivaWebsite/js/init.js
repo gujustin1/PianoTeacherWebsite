423f99d0599a0b65c9ebc2861aa09e3c91b3907c
Justin Gu <jjg282@cornell.edu>
2021-10-27 15:39:27 -0400

    Added website

---
+++ NivaWebsite/js/init.js
@@ -0,0 +1,83 @@
+
+
+  
+
+(function($){
+
+    $(document).ready(function(){
+    $('.modal').modal();
+  });
+    
+  $('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    indicators: true
+  });
+    
+$(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+
+  }); // end of document ready
+    
+
+  $(document).ready(function(){
+    $('.collapsible').collapsible();
+  });
+    
+  $(document).ready(function(){
+    $('.parallax').parallax();
+  });
+    
+  $(function(){
+
+    $('.sidenav').sidenav();
+
+  }); // end of document ready
+  
+})(jQuery); // end of jQuery name space
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+ 
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
